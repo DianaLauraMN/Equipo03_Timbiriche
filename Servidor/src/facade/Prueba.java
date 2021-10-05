7f7f4a8a333d0d144726e910e548a7ce299ca4d7
TorroSoft <torrozapata@gmail.com>
2021-10-05 15:14:56 -0700

    Componente servidor en proceso

---
+++ Servidor/src/facade/Prueba.java
@@ -0,0 +1,17 @@
+
+package facade;
+
+import java.util.ArrayList;
+import logica.Server;
+import objNegocios.Jugador;
+
+
+public class Prueba {
+    
+    public static void main(String[] args) {
+        
+       
+        
+        
+    }
+}
--- Servidor/src/logica/Server.java
+++ Servidor/src/logica/Server.java
@@ -31,5 +31,8 @@ public class Server {
         }
 
         return jugadores;
+        
+       
+        
     }
 }
